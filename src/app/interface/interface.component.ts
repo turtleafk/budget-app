f128867536e3b1264b54d4d2b011eb673950046e
Antonio <antonio330360@icloud.com>
2020-07-30 07:28:07 -0500

    Lost of styling, bootstrap

--- src/app/interface/interface.component.ts
+++ src/app/interface/interface.component.ts
@@ -2,14 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'interface',
-    templateUrl: 'interface.component.html'
+    templateUrl: 'interface.component.html',
+    styleUrls: [`interface.component.css`]
 })
 
 export class InterfaceComponent {
-    nameLabel = '';
-    numberLabel = '';
-
+    nameLabel = 'Type of Income';
+    numberLabel = 'Income Amount';
+    selectedOption = '';
+    
     getValue(event) {
-        console.log(event);
+        this.selectedOption = event.target.value;
+
+        if(this.selectedOption === 'expense') {
+            this.nameLabel = 'Type of Expense';
+            this.numberLabel = 'Expense Amount';
+        } else {
+            this.nameLabel = 'Type of Income';
+            this.numberLabel = 'Income Amount';
+        }
     }
 }
